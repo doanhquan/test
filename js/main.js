f8bdd39693646cc6241501ffff46530e5fcf55e4
doanhquan242 <doquan24021998@gmail.com>
2020-09-22 18:58:26 +0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+
+
+ // Owl-carousel
+   $(' .owl-carousel').owlCarousel({
+        loop: true,
+        autoplay: true,
+        dots: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            560: {
+                items: 2
+            }
+            
+           
+        }
+    })
+
+
+})
